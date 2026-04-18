7d14bd69275e16a56c992aa58a4f1c4ab2a4ff3d
nimosdev1 <nimos.dev.1@gmail.com>
2026-04-18 15:03:54 +0200

    reply_to email + sync toggle mode sombre

--- src/worker.js
+++ src/worker.js
@@ -1029,7 +1029,8 @@ function profileScreen(){
   <div class="card-title">\u{1F308} Apparence</div>
   <div class="row">
     <div class="row-body">
-      <div class="row-title">Mode clair</div>
+      <div class="row-title">Mode sombre</div>
+      <div class="row-sub">Interface plus reposante la nuit</div>
     </div>
     <label class="tg">
       <input type="checkbox" id="darkModeToggle" onchange="toggleDarkMode(this.checked)">
